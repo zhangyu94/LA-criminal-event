d52ade26b6a3a58322172756b22a0cc56f74e301
zhangyu94 <463568697@qq.com>
2017-01-23 12:24:58 +0800

    add storage

--- src/apps/Index/PageDemo/index.js
+++ src/apps/Index/PageDemo/index.js
@@ -119,7 +119,7 @@ export default {
 
       return itemSet
     },
-    calCloudData (data, topN, filter) {
+    calCloudData (data, topN) {
       let dict = {}
       for (let i = 0; i < data.length; ++i) {
         let curDescript = data[ i ].Descript
@@ -261,7 +261,7 @@ export default {
           // 'Resolution': { 'ARREST, BOOKED': 1 }
         }
         // console.log('filter-->', filter)
-        let cloudData = this.calCloudData(data, 10, [])
+        let cloudData = this.calCloudData(data, 10)
         this.wordleOption = {
           data: cloudData,
           wordCloudFont: 'Algerian',
@@ -312,107 +312,12 @@ export default {
         jud: true
       }
       // console.log(resolutionData)
-    },
-    getLineChartViewData () {
-      $.getJSON('/api/get_aqi_beijing', (data) => {
-        // console.log('air=>', data)
-        this.lineChartOption = {
-          title: {
-            text: 'Beijing AQI'
-          },
-          tooltip: {
-            trigger: 'axis'
-          },
-          xAxis: {
-            data: data.map(function (item) {
-              return item[ 0 ]
-            })
-          },
-          yAxis: {
-            splitLine: {
-              show: false
-            }
-          },
-          toolbox: {
-            left: 'center',
-            feature: {
-              dataZoom: {
-                yAxisIndex: 'none'
-              },
-              restore: {},
-              saveAsImage: {}
-            }
-          },
-          dataZoom: [ {
-            startValue: '2014-06-01'
-          }, {
-            type: 'inside'
-          } ],
-          visualMap: {
-            top: 10,
-            right: 10,
-            pieces: [ {
-              gt: 0,
-              lte: 50,
-              color: '#096'
-            }, {
-              gt: 50,
-              lte: 100,
-              color: '#ffde33'
-            }, {
-              gt: 100,
-              lte: 150,
-              color: '#ff9933'
-            }, {
-              gt: 150,
-              lte: 200,
-              color: '#cc0033'
-            }, {
-              gt: 200,
-              lte: 300,
-              color: '#660099'
-            }, {
-              gt: 300,
-              color: '#7e0023'
-            } ],
-            outOfRange: {
-              color: '#999'
-            }
-          },
-          series: {
-            name: 'Beijing AQI',
-            type: 'line',
-            data: data.map(function (item) {
-              return item[ 1 ]
-            }),
-            markLine: {
-              silent: true,
-              data: [ {
-                yAxis: 50
-              }, {
-                yAxis: 100
-              }, {
-                yAxis: 150
-              }, {
-                yAxis: 200
-              }, {
-                yAxis: 300
-              } ]
-            }
-          }
-        }
-      })
-    },
-    onChartClick (params) {
-      console.log('我收到了', params)
     }
   },
   ready () {
-    this.LOGS.log('PageDemo', this.token, storage, 'dfsfsd')
-    this.LOGS.log('SL.token', this.token, storage.get(this.token, true))
+    this.LOGS.log('SL.token', this.token, storage.get(this.token, false))
   },
   created () {
     this.getIncidentData()
-    // this.getLineChartViewData()
   }
 }
